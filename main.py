7eaa5ffd071150bee503f04d6e1184aa8db6ab6d
tdeng-zuel <3070053397@qq.com>
2021-03-21 10:45:23 +0800

    add arima

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.resnet import ResNet
-from utils.dataTool import train_val_test_loader, loadGridStrength, loadMigrationIndex, integrateMetaData
+from utils.dataTool import train_val_test_loader
 from utils.trainer import train_test_model, evaluate
-from utils.baseline import testLastHourRegression
+from utils.baseline import testLastHourRegression, testArima
 from config import Config
 import torch
 import numpy as np
@@ -13,9 +13,10 @@ config = Config()
 train_loader, val_loader = train_val_test_loader(config)
 # 定义网络，训练并测试
 net = ResNet(config).to(config.device)
-train_test_model(net, train_loader, val_loader, config)
+# train_test_model(net, train_loader, val_loader, config)
 # 测试baseline
 testLastHourRegression(val_loader, config.device, config.metric, hour_type='last_day')
 testLastHourRegression(val_loader, config.device, config.metric, hour_type='last_week')
 
+testArima(config)
 
--- utils/baseline.py
+++ utils/baseline.py
@@ -1,4 +1,9 @@
 import torch
+import pmdarima as pm
+import numpy as np
+from itertools import product
+import multiprocessing
+from tqdm.contrib.itertools import product
 
 
 def testLastHourRegression(test_iter, device, criterion, hour_type):
@@ -13,6 +18,31 @@ def testLastHourRegression(test_iter, device, criterion, hour_type):
             print('ERROR!')
             return None
 
+
+def onePlaceArima(train, test, test_len):
+    model = pm.auto_arima(train, trace=False, suppress_warnings=True)
+    pred = model.predict(test_len)
+    return np.sum(np.square(test - pred))
+
+
+def testArima(config):
+    flow = np.load('data/grid_graph_flow.npy')
+    flow = (flow - np.min(flow)) / (np.max(flow) - np.min(flow))
+    sample_num = flow.shape[0] - config.week * config.trend_len
+    train_len = int(sample_num * config.train_size)
+    test_len = sample_num - train_len
+
+    train_flow, test_flow = flow[:-test_len], flow[-test_len:]
+    data = [(train_flow[:, i, j], test_flow[:, i, j], test_len) for i, j in
+            product(range(train_flow.shape[1]), range(train_flow.shape[2]))]
+    err_sum = 0.0
+
+    cores = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=cores)
+    for err in pool.imap(onePlaceArima, data):
+        print(err)
+        err_sum += err
+    print(err_sum / test_len)
 
 
 
